f6746c94df50f09e457c83b089455ae44c28d7bd
qqdnada <nada.qori@gmail.com>
2019-12-11 17:33:19 +0800

    tambah main menu

---
+++ src/id/ac/its/finalprojectpbo/gui/ScoreManager.java
@@ -0,0 +1,44 @@
+package id.ac.its.finalprojectpbo.gui;
+
+import com.example.GameBoard;
+
+public class ScoreManager {
+	
+	// Current scores
+	private int currentScore;
+	private int currentTopScore;
+	private int[] board = new int[GameBoard.ROWS * GameBoard.COLS];
+	
+	private GameBoard gBoard;
+	
+	// Boolean
+	private boolean newGame;
+	
+	public ScoreManager(GameBoard gBoard) {
+		this.gBoard = gBoard;
+	}
+	
+	public void reset() {
+		newGame = true;
+		currentScore = 0;
+	}
+
+	public int getCurrentScore() {
+		return currentScore;
+	}
+
+	public void setCurrentScore(int currentScore) {
+		this.currentScore = currentScore;
+	}
+
+	public int[] getBoard() {
+		return board;
+	}
+
+	public boolean isNewGame() {
+		return newGame;
+	}
+
+	
+	
+}
